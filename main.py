ee4dec4f6dc0f7960aa8413c6db62a28aaf0d33b
Lu4nAlmeida <luantobias@yahoo.com.br>
2026-01-21 20:05:54 +0700

    First Functional Version

--- main.py
+++ main.py
@@ -1,45 +1,91 @@
 import requests
-import pprint
 import json
+from timesplits import filter_timeline, add_match_to_spreadsheet
+import os
+
 
 url = "https://mcsrranked.com/api"
 
-my_uuid = '5d851a87545645eb988079ced8ef4a6b'
+def get_user():
+    global user
 
-response = requests.get(url+'/users/MrLu4n/matches')
+    username = input("Enter your Minecraft nickname\nUsername: ")
+    response = requests.get(url + f'/users/{username}').json()
 
-# Check HTTP status
-response.raise_for_status()
+    if response["status"] == "success":
+        user = response['data']
+    else:
+        print("Player Not Found.")
 
-# Parse JSON response
-data = response.json()
-matches_id= []
+    with open('user.json', 'w') as f:
+        json.dump(user, f)
 
-for match in data['data']:
-    if match['date'] > 1768578040:
-        matches_id.append(match['id'])
+    return username
 
 
-matches_splits = []
-for id in matches_id:
-    splits = []
+def get_user_matches():
+    with open('user.json', 'r') as f:
+        user = json.load(f)
 
-    match = requests.get(url+f'/matches/{id}').json()['data']
+    user_id = user["uuid"]
+    matches = requests.get(url + f'/users/{user_id}/matches?count=50').json()
 
-    important_keys = ['id', 'players', 'result', 'forfeited', 'date', 'seedType', 'bastionType']
-    match_info = {key: match[key] for key in important_keys if key in match}  # Filters non-important keys
+    matches_id= []
+    for match in matches['data']:
+        if match['date'] > 1768578040: # TODO Make time period adjustable
+            matches_id.append(match['id'])
 
-    for split in match['timelines']:
-        if split['uuid'] == my_uuid:
-            splits.append(split)
 
-    match_info["splits"] = splits
-    matches_splits.append(match_info)
+    matches_splits = []
+    for id in matches_id:
+        splits = []
 
-with open('matches.json', 'w') as matches_file:
-    json.dump(data, matches_file, indent=4)
+        match = requests.get(url+f'/matches/{id}').json()['data']
 
-with open('recent_matches_splits.json', 'w') as matches_info_file:
-    json.dump(matches_splits, matches_info_file, indent=4)
+        important_keys = ['id', 'players', 'result', 'forfeited', 'date', 'seedType', 'bastionType']
+        match_info = {key: match[key] for key in important_keys if key in match}  # Filters non-important keys
 
-pprint.pprint(matches_splits[0])
+        # Filters only relevant achievements
+        splits.append(
+                filter_timeline(timeline=match['timelines'], uuid=user_id, detailed=True)
+        )
+
+        match_info["splits"] = splits
+        matches_splits.append(match_info)
+
+
+    with open('matches.json', 'w') as matches_file:
+        json.dump(matches, matches_file, indent=4)
+
+    with open('recent_matches_splits.json', 'w') as matches_info_file:
+        json.dump(matches_splits, matches_info_file, indent=4)
+
+
+def update_splits_sheets(user_id: str):
+    if os.path.exists("my_splits.csv"):
+        os.remove("my_splits.csv")
+
+    with open('recent_matches_splits.json', 'r') as f:
+        recent_matches = json.load(f)
+
+    for match in recent_matches:
+        add_match_to_spreadsheet(match, uuid=user_id)
+
+
+
+if __name__ == '__main__':
+    if not os.path.exists("user.json"):
+        username = get_user()
+    else:
+        with open("user.json", "r") as f:
+            user = json.load(f)
+        username = user['nickname']
+
+
+    print(f"Accessing {username}'s matches...")
+    get_user_matches()
+
+    print("Updating spreadsheets...")
+    update_splits_sheets(user['uuid'])
+
+    print("Done.")
--- timesplits.py
+++ timesplits.py
@@ -1,18 +1,91 @@
-import pprint
-import json
-
+import datetime
+import csv
+import os
 
 achievements = [
-    "story.lava_bucket",
     "story.enter_the_nether",
     "nether.find_bastion",
-    "nether.obtain_crying_obsidian",
     "nether.find_fortress",
-    "nether.obtain_blaze_rod",
     "projectelo.timeline.blind_travel",
     "story.follow_ender_eye",
-    "story.enter_the_end"
+    "story.enter_the_end",
+    "projectelo.timeline.dragon_death"
 ]
 
-def format_splits(timeline: list) -> list:
-    return timeline
+achievements_detailed = achievements + [
+    "story.lava_bucket",
+    "nether.obtain_crying_obsidian",
+    "nether.obtain_blaze_rod",
+]
+
+def filter_timeline(timeline: list, uuid: str, detailed: bool = False) -> list:
+    output = []
+
+    for achievement in timeline:
+        if detailed:
+            if achievement["uuid"] == uuid and achievement["type"] in achievements_detailed:
+                output.append(achievement)
+        else:
+            if achievement["uuid"] == uuid and achievement["type"] in achievements:
+                output.append(achievement)
+
+    return output
+
+
+def add_match_to_spreadsheet(match: dict, uuid: str, detailed: bool = False) -> dict:
+    timeline = filter_timeline(match["splits"][0], uuid, detailed)
+
+    if detailed:
+        fieldnames = [
+            "Overworld",
+            "Portal",
+            "Nether Nav",
+            "Bastion Route",
+            "Bartering",
+            "Fortress Nav",
+            "Blazes",
+            "Blind",
+            "Stronghold",
+            "End",
+        ]
+    else:
+        fieldnames = [
+            "Overworld",
+            "Nether Nav",
+            "Bastion",
+            "Fortress",
+            "Blind",
+            "Stronghold",
+            "End",
+        ]
+
+    splits = {name: 0 for name in fieldnames}
+
+    last_time = 0
+
+    for name in fieldnames:
+        try:
+            current_time = timeline.pop()["time"]
+            splits[name] = current_time - last_time
+            last_time = current_time
+        except IndexError:
+            splits[name] = None
+
+    fieldnames = fieldnames + ["Total", "Seed", "Bastion Type", "Won", "Forfeited"] # TODO Add date and opponent
+    splits["Total"] = match["result"]["time"]
+    splits["Seed"] = match["seedType"]
+    splits["Bastion Type"] = match["bastionType"]
+    splits["Won"] = True if match["result"]["uuid"] == uuid else False
+    splits["Forfeited"] = match["forfeited"]
+
+    # TODO Make the detailed version be saved on a separate file
+    with open("my_splits.csv", "a", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        # Write header only if file is empty
+        if os.stat("my_splits.csv").st_size == 0:
+            writer.writeheader()
+
+        writer.writerow(splits)
+
+    return splits
